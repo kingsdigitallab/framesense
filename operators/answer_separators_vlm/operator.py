c900c05f8834c7da5778426f1a2b1615348450f6
geoffroy-noel-ddh <geoffroy.noel@kcl.ac.uk>
2026-09-12 02:22:19 +0100

    fix(answer_separators_vlm): nested quotation marks in string

--- operators/answer_separators_vlm/operator.py
+++ operators/answer_separators_vlm/operator.py
@@ -207,7 +207,7 @@ class AnswerSeparatorsVLM(answer_videos_vlm_operator.AnswerVideosVLM):
             answer = lists[0] if lists else None
 
         if not isinstance(answer, list):
-            errors.append(f'No list of separators could be parsed from the model answer ({response['result']})')
+            errors.append(f'No list of separators could be parsed from the model answer ({response["result"]})')
             self._warn(f'No list of separators could be parsed from the model answer, chunk ignored ({self.get_hhmmss(start_secs)}): {response["result"]}')
             answer = []
 
